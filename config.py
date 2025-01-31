8ad4805c254f6a0f41038ee92e21ea112410d1cf
Nada Zakaria <138094384+NadaZakaria99@users.noreply.github.com>
2025-01-31 22:07:40 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 # config.py
-# API_KEY = "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"
-API_KEY = "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"
-PDF_FOLDER_PATH = r"C:\Users\nz390\Desktop\clicklaptop\clicklaptop\Ram\ram"
+API_KEY = "YOUR_NVIDIA_API_KEY"
+PDF_FOLDER_PATH = r"YOUR_PDF_FOLDER_PATH"
